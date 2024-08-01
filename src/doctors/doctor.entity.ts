7d7aec769d45151aa9dbde8591d15e4c4ea8ea2a
Joshmatjjen <joshmatparrot@gmail.com>
2024-08-01 20:58:47 +0100

    Setup nestjs for typescript and create doctor entiy

---
+++ src/doctors/doctor.entity.ts
@@ -0,0 +1,13 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class Doctor {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  specialty: string;
+}
